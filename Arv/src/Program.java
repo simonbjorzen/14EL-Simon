5bd7b22b97aa6a4a5e20956a8612bdfda54da4fa
Unknown <thatsmiley14@gmail.com>
2016-10-26 10:47:47 +0200

    Cleanup

--- Arv/src/Program.java
+++ Arv/src/Program.java
@@ -16,95 +16,87 @@ public class Program {
         System.out.println("2. Calculate the gas usage of your vehicle.");
         int ans;
         Scanner reader = new Scanner(System.in);
-        ans=reader.nextInt();
-        switch (ans)
-        {
-            case 1:
-            {
+        ans = reader.nextInt();
+        switch (ans) {
+            case 1: {
                 System.out.println("Which vehicle is this sale on?");
                 System.out.println(" ");
                 System.out.println("1. The Bicycle");
                 System.out.println("2. The Car");
                 System.out.println("3. The Truck");
                 int ans1;
-                ans1=reader.nextInt();
-                switch(ans1)
-                {
-                    case 1:
-                    {
+                ans1 = reader.nextInt();
+                switch (ans1) {
+                    case 1: {
                         System.out.println("What is the percentage of the sale?");
-                        double percent=reader.nextInt();
-                        double percent1=percent/100;
-                        double price=bike.price*percent1;
-                        double finalprice=bike.price-price;
+                        double percent = reader.nextInt();
+                        double percent1 = percent / 100;
+                        double price = bike.price * percent1;
+                        double finalprice = bike.price - price;
                         System.out.print("Priset efter rabatten blir ");
                         System.out.print(finalprice);
                         System.out.print(" kronor.");
                         System.out.println("");
                         break;
                     }
-                    case 2:
-                    {
+                    case 2: {
                         System.out.println("What is the percentage of the sale?");
-                        double percent=reader.nextInt();
-                        double percent1=percent/100;
-                        double price=sedan.price*percent1;
-                        double finalprice=sedan.price-price;
+                        double percent = reader.nextInt();
+                        double percent1 = percent / 100;
+                        double price = sedan.price * percent1;
+                        double finalprice = sedan.price - price;
                         System.out.print("Priset efter rabatten blir ");
                         System.out.print(finalprice);
                         System.out.print(" kronor.");
                         System.out.println("");
                         break;
                     }
-                    case 3:
-                    {
+                    case 3: {
                         System.out.println("What is the percentage of the sale?");
-                        double percent=reader.nextInt();
-                        double percent1=percent/100;
-                        double price=freighter.price*percent1;
-                        double finalprice=freighter.price-price;
+                        double percent = reader.nextInt();
+                        double percent1 = percent / 100;
+                        double price = freighter.price * percent1;
+                        double finalprice = freighter.price - price;
                         System.out.print("Priset efter rabatten blir ");
                         System.out.print(finalprice);
                         System.out.print(" kronor.");
                         System.out.println("");
+                        break;
+                    }
+                    default: {
+                        System.out.println("Error!");
                         break;
                     }
                 }
 
             }
-            case 2:
-            {
+            case 2: {
                 System.out.println("Which vehicle do you want to know the gas usage of?");
                 System.out.println(" ");
                 System.out.println("1. The Bicycle");
                 System.out.println("2. The Car");
                 System.out.println("3. The Truck");
                 int ans2;
-                ans2=reader.nextInt();
-                switch(ans2)
-                {
-                    case 1:
-                    {
-                        System.out.println(bike.gasusage+" liters/10km");
+                ans2 = reader.nextInt();
+                switch (ans2) {
+                    case 1: {
+                        System.out.println(bike.gasusage + " liters/10km");
                         break;
                     }
-                    case 2:
-                    {
-                        System.out.println(sedan.gasusage+" liters/10km");
+                    case 2: {
+                        System.out.println(sedan.gasusage + " liters/10km");
                         break;
                     }
-                    case 3:
-                    {
-                        System.out.println(freighter.gasusage+" liters/10km");
+                    case 3: {
+                        System.out.println(freighter.gasusage + " liters/10km");
                         break;
                     }
-                    default:
-                    {
+                    default: {
                         System.out.println("Error!");
+                        break;
                     }
                 }
             }
-            break;
         }
     }
 
